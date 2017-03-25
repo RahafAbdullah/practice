6e4b3def86740a6038f734cc4f6258300d20dcc4
Hulul <Hulul@HULUL-PC>
2017-03-25 20:20:15 +0300

    adding

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,17 @@ namespace ConsoleApp1
         {
             //This is Myfirst programm
             Console.WriteLine(" Hello World ");
+            float x = float.MinValue;
+            short y = short.MaxValue;
+            int z = int.MaxValue;
+            Console.WriteLine(" float {0} \n short {1} \n int {2} ", x, y, z);
+            float a = 1f / 3;
+            double b = 1d / 3;
+            decimal c = 1m / 3;
+            decimal d = 1m / 3;
+            Console.WriteLine(" float {0} \n double {1} \n decimal {2} \n decimal2 {3}", a, b, c,d);
+            Console.ReadLine();
+
         }
     }
 }
